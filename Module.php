b6a78c4553a8e190fe7cb14a680fe7a0fd1530a5
Julian Maurice <julian.maurice@biblibre.com>
2023-08-31 15:30:48 +0200

    Avoid "A new entity was found" exception by using a reference

--- Module.php
+++ Module.php
@@ -13,6 +13,7 @@ use Omeka\Api\Adapter\ItemSetAdapter;
 use Omeka\Api\Adapter\MediaAdapter;
 use Omeka\Entity\Item;
 use Omeka\Entity\Media;
+use Omeka\Entity\Property;
 use Omeka\Entity\Resource;
 use Omeka\Entity\Value;
 use Quark\Form\ConfigForm;
@@ -87,6 +88,7 @@ class Module extends AbstractModule
         $services = $this->getServiceLocator();
         $settings = $services->get('Omeka\Settings');
         $arkManager = $services->get('Quark\ArkManager');
+        $em = $services->get('Omeka\EntityManager');
 
         $values = $resource->getValues();
         $arkValues = $arkManager->getArkValues($resource);
@@ -111,9 +113,10 @@ class Module extends AbstractModule
             }
 
             if ($ark) {
+                $dctermsIdentifierProperty = $em->getReference(Property::class, $arkManager->getDctermsIdentifierProperty()->getId());
                 $value = new Value();
                 $value->setResource($resource);
-                $value->setProperty($arkManager->getDctermsIdentifierProperty());
+                $value->setProperty($dctermsIdentifierProperty);
                 $value->setType('literal');
                 $value->setValue($ark);
                 $values->add($value);
